0bcdb9265aef93029fbdac39cb90c967f39a1112
samofrench <sam.o.french@gmail.com>
2016-01-15 16:24:54 -0800

    friday afternoon

--- app/app.js
+++ app/app.js
@@ -8,8 +8,8 @@ app.config([
 			templateUrl: "views/index.html",
 		}).when("/about", {
 			templateUrl: "views/about.html",
-		}).when("/projects", {
-			templateUrl: "views/projects.html"
+		}).when("/portfolio", {
+			templateUrl: "views/portfolio.html"
 		}).when("/blog", {
 			templateUrl: "views/blog.html"
 		}).when("/contact", {
