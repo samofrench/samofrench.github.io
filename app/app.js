110d9811c37a3b0e64452c349c6b74ad0474558b
samofrench <sam.o.french@gmail.com>
2016-03-11 15:27:26 -0800

    sw project order

--- app/app.js
+++ app/app.js
@@ -23,6 +23,37 @@ app.controller('HomeCtrl', ['$scope', '$uibModal', function($scope, $uibModal) {
 
   $scope.projects = [
     {
+      name: "Concrt", 
+      descriptionShort: "A place to discover and share local music",
+      technologies: "Ruby on Rails, D3.js, Bootstrap, HTML5, CSS3, PostgreSQL",
+      role: "Front-end data visualization, full stack developer",
+      team: [{name: "Chris Carlson", gh: "https://github.com/TheCodingCarlson"}, {name: "Beth Ernsberger", gh: "https://github.com/bethburger42"}],
+      descriptionLong: "The concept behind the Concrt app is a social site where users are able to find and share concerts with friends. Our main focus was to create a product that was user friendly with a lot of functionality. Users use the site to search for concerts and can also add them to their personal calendar. Users can follow each other and view their personal concert calendars. The JamBase API is used to pull concert information based on location and dates. We used D3.js to render each user's calendar with the events that they have added.",
+      url: "http://concrt.herokuapp.com/",
+      github: "https://github.com/TheCodingCarlson/concrt",
+      mainImg: "../images/concrt/home.png",
+      desktopImages: [
+        "../images/concrt/profile.png",
+        "../images/concrt/concerts.png",
+        "../images/concrt/network.png",
+        "../images/concrt/signin.png"  
+      ]
+    },
+    {
+      name: "Penny Post", 
+      descriptionShort: "Send a postcard using photos from your Instagram and Facebook accounts",
+      technologies: "Angular.js, Node.js, Express.js, MongoDB, HTML5, CSS3",
+      role: "Front-end engineer, full stack developer",
+      team: [{name: "Nathan Maas", gh: "https://github.com/Nathanmaas"}],
+      descriptionLong: "Penny post is an app that lets you send your own Instagram and Facebook photos as physical postcards in the mail. We created a simple, intuitive user interface that makes designing the card as easy and fast as possible. Users log in and choose photos from their social media accounts powered by APIs, write a personal message, and enter the recipients' addresses. In a few days, your recipients will receive a custom-printed postcard in the mail.",
+      mainImg: "../images/pennypost/pennypost.png",
+      desktopImages: [
+        "../images/pennypost/postcard-front.png",
+        "../images/pennypost/postcard-back.png",
+        "../images/pennypost/pp-create.png"  
+      ]
+    },
+    {
       name: "Cultivate", 
       descriptionShort: "Connecting gardeners with their plants",
       technologies: "Angular.js, Node.js, Express.js, MongoDB, D3.js, HTML5, CSS3",
@@ -42,37 +73,6 @@ app.controller('HomeCtrl', ['$scope', '$uibModal', function($scope, $uibModal) {
       ]
     },
     {
-      name: "Penny Post", 
-      descriptionShort: "Send a postcard using photos from your Instagram and Facebook accounts",
-      technologies: "Angular.js, Node.js, Express.js, MongoDB, HTML5, CSS3",
-      role: "Front-end engineer, full stack developer",
-      team: [{name: "Nathan Maas", gh: "https://github.com/Nathanmaas"}],
-      descriptionLong: "Penny post is an app that lets you send your own Instagram and Facebook photos as physical postcards in the mail. We created a simple, intuitive user interface that makes designing the card as easy and fast as possible. Users log in and choose photos from their social media accounts powered by APIs, write a personal message, and enter the recipients' addresses. In a few days, your recipients will receive a custom-printed postcard in the mail.",
-      mainImg: "../images/pennypost/pennypost.png",
-      desktopImages: [
-        "../images/pennypost/postcard-front.png",
-        "../images/pennypost/postcard-back.png",
-        "../images/pennypost/pp-create.png"  
-      ]
-    },
-    {
-      name: "Concrt", 
-      descriptionShort: "A place to discover and share local music",
-      technologies: "Ruby on Rails, D3.js, Bootstrap, HTML5, CSS3, PostgreSQL",
-      role: "Front-end data visualization, full stack developer",
-      team: [{name: "Chris Carlson", gh: "https://github.com/TheCodingCarlson"}, {name: "Beth Ernsberger", gh: "https://github.com/bethburger42"}],
-      descriptionLong: "The concept behind the Concrt app is a social site where users are able to find and share concerts with friends. Our main focus was to create a product that was user friendly with a lot of functionality. Users use the site to search for concerts and can also add them to their personal calendar. Users can follow each other and view their personal concert calendars. The JamBase API is used to pull concert information based on location and dates. We used D3.js to render each user's calendar with the events that they have added.",
-      url: "http://concrt.herokuapp.com/",
-      github: "https://github.com/TheCodingCarlson/concrt",
-      mainImg: "../images/concrt/home.png",
-      desktopImages: [
-        "../images/concrt/profile.png",
-        "../images/concrt/concerts.png",
-        "../images/concrt/network.png",
-        "../images/concrt/signin.png"  
-      ]
-    }, 
-    {
       name: "Paidit!", 
       descriptionShort: "Record-keeping app for renters",
       technologies: "Angular.js, Bootstrap, HTML5, CSS3",
@@ -87,7 +87,6 @@ app.controller('HomeCtrl', ['$scope', '$uibModal', function($scope, $uibModal) {
         "../images/paidit/phone-paidit.png"  
       ]
     },
-
   ];
 
   $scope.open = function(project) {
